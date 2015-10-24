247fa21fb35bf644b2fc659d512f0a527e290527
EPM <edwardmurakami1@edwards-macbook-pro.local>
2015-10-24 13:11:56 -0400

    Hope page initial commit

---
+++ searchingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  searchingViewController.swift
+//  BeaconScanner
+//
+//  Created by EPM on 2015-10-24.
+//  Copyright © 2015 Names Are Hard. All rights reserved.
+//
+
+import UIKit
+
+class searchingViewController: UIViewController {
+
+}
