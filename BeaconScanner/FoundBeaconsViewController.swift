36aafb23b2e117a9495f17d5f0f8719fdd8ddec7
Sam Deakin <sam_d1995@hotmail.com>
2015-10-24 12:15:18 -0400

    Added basic tableView and controller file. Should move to Main.storyboard after launch

---
+++ BeaconScanner/FoundBeaconsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  FoundBeaconsView.swift
+//  BeaconScanner
+//
+//  Created by Sam Deakin on 2015-10-24.
+//  Copyright © 2015 Names Are Hard. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FoundBeaconsViewController: UITableViewController {
+    
+    override init(style: UITableViewStyle) {
+        super.init(style: style);
+        
+        // Add extra initialization here
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        // TODO this is probably important
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // This is assuming every beacon will take up 1 cell so we only need one section to deal with each beacon
+        return 1;
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        // Currently no table section headers
+        return nil;
+    }
+    
+    override func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        // Curretnly no table section footers
+        return nil;
+    }
+}
