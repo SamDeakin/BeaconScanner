671c26f4e5e27e3126f6ed0cf29f4e579919fbbc
Ha Phuong Vu <me@phuong.vu>
2015-10-24 20:19:01 -0400

    Add classes

---
+++ BeaconScanner/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  BeaconScanner
+//
+//  Created by Ha Phuong Vu on 2015-10-24.
+//  Copyright © 2015 Names Are Hard. All rights reserved.
+//
+
+import UIKit
+
+class User {
+  
+}
